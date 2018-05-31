b9122b9143b013676a0aca68b3d5d234d4fb52de
Matthew Davis <matt.davis@xterraglobal.com>
2018-05-30 18:53:11 -0700

    Another commit

---
+++ Block/Hotspot.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dmatthew\Hotspot\Block;
+
+/**
+ * Hotspot Widget
+ */
+class Hotspot extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
+{
+
+}
